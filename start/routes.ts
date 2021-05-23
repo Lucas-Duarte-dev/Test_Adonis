23b66fe5660cb4f2b3d69cb420d4261a0981140a
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-22 22:51:52 -0300

    Use resource for make all routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,3 +1,3 @@
 import Route from "@ioc:Adonis/Core/Route";
 
-Route.resource("users", "UserController.index").except(["update"]).apiOnly();
+Route.resource("users", "UsersController").except(["update"]).apiOnly();
