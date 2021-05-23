14b17c701bef9e2da195eb7e6e86bb58c93db12b
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-22 22:45:52 -0300

    Created CRUD for fake users

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,20 +1,34 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class UsersController {
-  public async index() {
-    return [
-      {
-        name: 'Lucas',
-        email: 'test@test.com',
-        phone: '865468165',
-        password: 'test',
-      },
-      {
-        name: 'Jorge',
-        email: 'test1@test.com',
-        phone: '865468165',
-        password: 'test21',
-      },
-    ]
+  private static users = [
+    {
+      id: 1,
+      name: "Lucas",
+      email: "test@test.com",
+    },
+    {
+      id: 2,
+      name: "Jorge",
+      email: "test1@test.com",
+    },
+  ];
+
+  public async index({ request }: HttpContextContract) {
+    return UsersController.users;
+  }
+  public async store({ request }: HttpContextContract) {
+    const data: { name: string; email: string } = request.only([
+      "name",
+      "email",
+    ]);
+    const newId = UsersController.users.length + 1;
+    const user = {
+      id: newId,
+      name: data.name,
+      email: data.email,
+    };
+    UsersController.users.push(user);
+    return user;
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,3 @@
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route";
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.resource("users", "UserController.index").except(["update"]).apiOnly();
