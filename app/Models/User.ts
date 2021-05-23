c2c9ef81bde5fc50158096ae7c7e613772cc0f85
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-23 16:56:44 -0300

    Change column id with type int for uuid

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -3,7 +3,7 @@ import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
-  public id: number;
+  public id: string;
 
   @column()
   public name: string;
--- database/migrations/1621794687575_users.ts
+++ database/migrations/1621794687575_users.ts
@@ -4,8 +4,10 @@ export default class Users extends BaseSchema {
   protected tableName = "users";
 
   async up() {
+    this.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("id");
+      table.uuid("id").primary().defaultTo(this.raw("uuid_generate_v4()"));
       table.string("name").notNullable();
       table.string("email").notNullable().unique();
       table.string("phone").notNullable();
@@ -16,5 +18,6 @@ export default class Users extends BaseSchema {
 
   public async down() {
     this.schema.dropTable(this.tableName);
+    this.schema.raw('DROP EXTENSION IF EXISTS "uuid-ossp"');
   }
 }
