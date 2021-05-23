c6b044e8b3a7675d15f4a9344028ff02523b003d
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-23 16:07:00 -0300

    Change migration and model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -3,7 +3,7 @@ import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
-  public id: string;
+  public id: number;
 
   @column()
   public name: string;
--- database/migrations/1621794687575_users.ts
+++ database/migrations/1621794687575_users.ts
@@ -3,9 +3,9 @@ import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 export default class Users extends BaseSchema {
   protected tableName = "users";
 
-  public async up() {
+  async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.uuid("id").primary();
+      table.increments("id");
       table.string("name").notNullable();
       table.string("email").notNullable().unique();
       table.string("phone").notNullable();
