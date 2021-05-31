758345562a6b29579da8d00e4202924b5282874d
webjump257 <lucasduarteb2000@hotmail.com>
2021-05-31 19:30:34 -0300

    Created model complements

--- app/Models/Complement.ts
+++ app/Models/Complement.ts
@@ -1,13 +1,28 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
 export default class Complement extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: string;
+
+  @column()
+  public garage: boolean;
+
+  @column()
+  public bedrooms: number;
+
+  @column()
+  public bathrooms: number;
+
+  @column()
+  public size: number;
+
+  @column()
+  public post_id: string;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 }
